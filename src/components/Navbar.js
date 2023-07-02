a4bd2cd93e8116d82bb7476bc570401a5207735f
Sebastian Szaładyga <sebastian.szaladyga@gmail.com>
2023-07-02 21:52:19 +0200

    navbar implemented

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import { Link, useLocation } from "react-router-dom";
+import "../styles/Navbar.css";
+
+import { useState, useEffect } from "react";
+
+
+function Navbar() {
+    const [expandNavbar, setExpandNavbar] = useState(false);
+    const location = useLocation();
+
+    useEffect(() => {
+        setExpandNavbar(false)
+    }, [location])
+
+    return (
+        <div className="navbar" id={expandNavbar ? "open" : "close"}>
+            <div className="toggleButton">
+                <button
+                    onClick={() => setExpandNavbar(expandNavbar => !expandNavbar)}
+                >
+                   
+                </button>
+            </div>
+            <div className="links">
+                <Link to="/">Home</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/experience">Experience</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
