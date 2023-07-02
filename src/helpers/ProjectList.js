2cfa6a7e83919f5a6faab6c862f531c242504b71
Sebastian Szaładyga <sebastian.szaladyga@gmail.com>
2023-07-02 21:53:03 +0200

    project list added

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,32 @@
+import Proj1 from "../assets/proj1.png";
+import Proj2 from "../assets/proj2.png";
+import Proj4 from "../assets/proj4.png";
+import Proj3 from "../assets/proj3.png";
+
+export const ProjectList = [
+    {
+        name: "To-do list",
+        image: Proj1,
+        skills: "React, JS (ES6), HTML, CSS",
+        link: "https://sebastian092.github.io/todo-list-react/",
+    },
+    {
+        name: "Currency calculator",
+        image: Proj2,
+        skills: "React, JS (ES6), HTML, CSS",
+        link: "https://sebastian092.github.io/currency-calculator-react/",
+    },
+    {
+        name: "Pig game",
+        image: Proj3,
+        skills: "JavaScript, HTML, CSS",
+        link: "https://sebastian092.github.io/Pig-Game/"
+    },
+    {
+        name: "Guess my number",
+        image: Proj4,
+        skills: "JavaScript, HTML, CSS",
+        link: "https://sebastian092.github.io/Guess-My-Number/"
+    },
+]
+
